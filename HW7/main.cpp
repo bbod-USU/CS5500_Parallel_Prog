881a016341f26245427a84217e197fbc061b3efa
Brady <bradybodily@Bradys-iMac-2.local>
2020-03-16 22:17:46 -0600

    working

--- HW7/main.cpp
+++ HW7/main.cpp
@@ -1,18 +1,13 @@
 #include<iostream>
 #include<mpi.h>
 #include<stdlib.h>
-#include<unistd.h>
 #include<time.h>
 #include<queue>
 #define MCW MPI_COMM_WORLD
-#define ANY MPI_ANY_SOURCE
-#define IGNORE MPI_STATUS_IGNORE
-#define BLACK 0
-#define WHITE 1
+#define GENERIC 0
+#define SPECIAL 1
 #define JOB 1
 #define TOKEN 2
-#define CREATED 3
-#define COMPLETED 4
 
 using namespace std;
 
@@ -22,8 +17,8 @@ int main(int argc, char **argv) {
     int tasksCreated = 0;
     int tasksCompleted = 0;
     int taskLimit;
-    int tokenColor = WHITE;
-    int myColor = WHITE;
+    int tokenValue = SPECIAL;
+    int myToken = SPECIAL;
     int newTask;
     bool hasToken = false;
     queue<int> myTasks;
@@ -44,18 +39,15 @@ int main(int argc, char **argv) {
         MPI_Send(&job, 1, MPI_INT, rand() % 4, JOB, MCW); //Send out the initial job
     }
 
-    while(1) {
-        MPI_Probe(ANY,JOB,MCW,&status);
-        MPI_Recv(&newTask,1,MPI_INT,ANY,JOB,MCW,&status);
-        myTasks.push(newTask);
-        //MPI_IProbe(ANY, JOB, MCW, &jobFlag, &status); //Check to see if a job is available
-//        if(jobFlag) {
-//            MPI_Irecv(&newTask, 1, MPI_INT, ANY, JOB, MCW, &request);
-//            myTasks.push(newTask);
-//        }
+    while(true) {
+        MPI_Iprobe(MPI_ANY_SOURCE, JOB, MCW, &jobFlag, &status); //Check to see if a job is available
+        if(jobFlag) {
+            MPI_Irecv(&newTask, 1, MPI_INT, MPI_ANY_SOURCE, JOB, MCW, &request);
+            myTasks.push(newTask);
+        }
         if(newTask == -1) { //If the stop signal is received, send my info to process 0 and break out of the loop
-            MPI_Send(&tasksCreated, 1, MPI_INT, 0, CREATED, MCW);
-            MPI_Send(&tasksCompleted, 1, MPI_INT, 0, COMPLETED, MCW);
+            MPI_Send(&tasksCreated, 1, MPI_INT, 0, size-1, MCW);
+            MPI_Send(&tasksCompleted, 1, MPI_INT, 0, size, MCW);
             break;
         }
         if(myTasks.size() > 16) { //If my task queue is greater than size 16, send off 2 tasks
@@ -63,59 +55,67 @@ int main(int argc, char **argv) {
                 int task = myTasks.front();
                 myTasks.pop();
                 int dest = rand() % 4;
-                while(dest == rank) {dest = rand() % 4;}
-                if(dest < rank) {myColor = BLACK;}
+                while(dest == rank) {dest = rand() % size;}
+                if(dest < rank) { myToken = GENERIC;}
                 MPI_Send(&task, 1, MPI_INT, dest, JOB, MCW);
             }
         }
-        if(!myTasks.empty()) { //Perform a task
+        if(!myTasks.empty()) {
             int num = myTasks.front();
             myTasks.pop();
-            sleep(static_cast<double>(num)/5);
+            num^3-num^4;
             tasksCompleted++;
         }
-        if(tasksCreated < taskLimit) { //Create more tasks
-            for(int i = 0; i < rand() % 3 + 1; i++) {
-                myTasks.push(rand() % 3 + 1);
+        if(tasksCreated < taskLimit) {
+            for(int i = 0; i < rand() % size; i++) {
+                myTasks.push(rand() % size);
                 tasksCreated++;
             }
         }
         if(myTasks.empty()) { //Check the token information
             if(hasToken) {
-                cout<<"Rank: "<<rank<<", token: "<<(tokenColor==BLACK ? "Black" : "White")<<endl;
+                string tmp =  "";
+                if(tokenValue == GENERIC)
+                    tmp = "Generic";
+                else
+                    tmp = "Special";
+
+
+                cout << "My Rank: " << rank << "     My token: " << tmp << endl;
                 int dest = rank + 1 < size ? rank + 1 : 0;
-                if(rank == 0) {tokenColor = WHITE;}
-                MPI_Send(&tokenColor, 1, MPI_INT, dest, TOKEN, MCW);
+                if(rank == 0) { tokenValue = SPECIAL;}
+                MPI_Send(&tokenValue, 1, MPI_INT, dest, TOKEN, MCW);
                 hasToken = false;
-                myColor = WHITE;
+                myToken = SPECIAL;
             }
             else {
                 int src = rank - 1 >= 0 ? rank - 1 : size - 1;
                 MPI_Iprobe(src, TOKEN, MCW, &tokenFlag, &status);
                 if(tokenFlag) {
-                    MPI_Irecv(&tokenColor, 1, MPI_INT, src, TOKEN, MCW, &request);
+                    MPI_Irecv(&tokenValue, 1, MPI_INT, src, TOKEN, MCW, &request);
                     hasToken = true;
-                    if(rank == 0 && tokenColor == WHITE) {
+                    if(rank == 0 && tokenValue == SPECIAL) {
                         int globalStop = -1;
                         for(int i = 1; i < size; i++) {
                             MPI_Send(&globalStop, 1, MPI_INT, i, JOB, MCW);
                         }
                         break;
                     }
-                    if(myColor == BLACK) {
-                        tokenColor = BLACK;
+                    if(myToken == GENERIC) {
+                        tokenValue = GENERIC;
                     }
                 }
             }
         }
     }
-
-    if(rank == 0) { //Print out the task information
-        int created, completed;
+//Print info after run.
+    if(rank == 0) {
+        int created;
+        int completed;
         cout<<"Rank 0 created "<<tasksCreated<<" tasks and completed "<<tasksCompleted<<" tasks"<<endl;
-        for(int i = 1; i < size; i++) {
-            MPI_Recv(&created, 1, MPI_INT, i, CREATED, MCW, IGNORE);
-            MPI_Recv(&completed, 1, MPI_INT, i, COMPLETED, MCW, IGNORE);
+        for(int i = 0; i < size; i++) {
+            MPI_Recv(&created, 1, MPI_INT, i, size -1, MCW, MPI_STATUS_IGNORE);
+            MPI_Recv(&completed, 1, MPI_INT, i, size, MCW, MPI_STATUS_IGNORE);
             cout<<"Rank "<<i<<" created "<<created<<" tasks and completed "<<completed<<" tasks"<<endl;
             tasksCreated += created;
             tasksCompleted += completed;
