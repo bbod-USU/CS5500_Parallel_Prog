00b777314764b3b779094727428faca8c4521521
bbod <bradybod@gmail.com>
2020-03-30 15:35:29 -0600

    Auto Commit

--- HW10/MakePermutationMatrix.cpp
+++ HW10/MakePermutationMatrix.cpp
@@ -37,6 +37,10 @@ std::vector<double> MakePermutationMatrix::MakePermutation(std::vector<City> sou
     std::cout << std::endl;
     do {
         double cost = 0;
+        std::cout << "source: ";
+        for(int i = 0; i < myPerm.size(); i++){
+            std::cout << myPerm[i].GetName() << " ";
+        }
         //Add up the cost of the current permutation.
         for (int i = 1; i < myPerm.size(); i++) {
             cost += costMatrix[myPerm[i].GetName()][myPerm[i-1].GetName()];
