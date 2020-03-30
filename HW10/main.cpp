257e56d17b61233823bd055b4bcff0bbbe38f3c6
bbod <bradybod@gmail.com>
2020-03-30 14:28:22 -0600

    Auto Commit

--- HW10/main.cpp
+++ HW10/main.cpp
@@ -50,6 +50,8 @@ int main(int argc, char* argv[])
     std::cout << "Rank: " << rank << " IM HERE 1" << std::endl;
     MPI_Bcast(&flatMatrix[0], flatMatrix.size(), MPI_DOUBLE, 0, MPI_COMM_WORLD);
     std::cout << "Rank: " << rank << " IM HERE 2" << std::endl;
+    std::cout << "Rank: " << rank << " citiesSize: "<< citiesSize << std::endl;
+
     MPI_Bcast(&serializedCity[0], citiesSize*3, MPI_INT, 0, MPI_COMM_WORLD);
     std::cout << "Rank: " << rank << " IM HERE 3" << std::endl;
 
