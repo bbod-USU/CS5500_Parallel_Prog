50e42a2fd2c0219e4ceef840ada9af98184edc79
bbod <bradybod@gmail.com>
2020-04-11 16:45:17 -0600

    Final Project start

---
+++ FinalProject/cmake-build-debug/CMakeFiles/HW10.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/bradybodily/Repositories/CS5500_Parallel_Programming/FinalProject/main.cpp" "/Users/bradybodily/Repositories/CS5500_Parallel_Programming/FinalProject/cmake-build-debug/CMakeFiles/HW10.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "OMPI_SKIP_MPICXX"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/Cellar/open-mpi/4.0.2/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ FinalProject/cmake-build-debug/CMakeFiles/HW10.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HW10.dir/main.cpp.o"
+  "HW10"
+  "HW10.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HW10.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
